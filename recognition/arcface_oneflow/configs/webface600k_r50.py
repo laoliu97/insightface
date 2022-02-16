26eacd6d7e434e5114ebacd66043c80c38badf68
olojuwin <502401452@qq.com>
2022-02-16 16:18:59 +0800

    simplify readme.py

---
+++ recognition/arcface_oneflow/configs/webface600k_r50.py
@@ -0,0 +1,30 @@
+from easydict import EasyDict as edict
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.loss = "cosface"
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.partial_fc = 1
+config.sample_rate = 0.2
+config.model_parallel = True
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 128
+config.lr = 0.1  # batch size is 512
+
+config.dataset = "webface600k"
+config.ofrecord_path = "/data/disk1/zhuwang/face_data/WebFace260M_full/webface600k/ofrecord"
+config.ofrecord_part_num = 32
+config.num_classes = 617832
+config.num_image = 12724913
+config.num_epoch = 20
+config.warmup_epoch = -1
+config.decay_epoch = [8, 12, 15, 18]
+config.val_targets = []
